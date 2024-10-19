b9ab246a64a0f13f80a2c9c0abd9c8ac2721efea
rafacouceiro <rafael.balseiro@udc.es>
2024-10-19 17:39:43 +0200

    renamed modules and created main module

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from userInterface import MainWindow
+import sys
+from PySide6 import QApplication
+
+"""
+This is the main module of the application.
+In charge of running the app.
+"""
+
+def main():
+    # Main entry point of the application
+    app = QApplication(sys.argv)
+
+    # Create and show the FileExplorer widget
+    interface = MainWindow()
+    interface.show()
+
+    # Start the application's event loop
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
+    
