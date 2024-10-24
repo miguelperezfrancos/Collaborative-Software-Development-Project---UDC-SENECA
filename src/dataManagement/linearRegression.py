56cf38584a9c8b21d7fcead674c405fe734b5113
rafacouceiro <rafael.balseiro@udc.es>
2024-10-24 14:27:31 +0200

    started to code regression module

--- src/dataManagement/linearRegression.py
+++ src/dataManagement/linearRegression.py
@@ -1,3 +1,26 @@
 """
-This module will create a linear regression usinc scikit learn
+This module will create a linear regression using scikit learn
 """
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+class Regression():
+
+    def __init__():
+        pass
+
+    def make_model(self, input_col, output_col):
+
+        # set the target and feature values
+        x = df[['median_income']]
+        y = df['median_house_value']
+
+        # create and fit the model
+        model = LinearRegression()
+        model.fit(x, y)
+        y_pred = model.predict(X=x)
+
